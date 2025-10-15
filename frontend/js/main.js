e50e104e7785cdc4f872d9fa5596ba8e182469b0
Yannick Franchaisse <yannick.franchaisse@gmail.com>
2025-10-15 19:19:16 +0200

    CSS index.html header

---
+++ frontend/js/main.js
@@ -0,0 +1,13 @@
+
+// Variable Ruban choix de la langue
+//-------------------------------------
+
+  let choixLangue = "FRANCAIS (Fr)";
+  document.getElementById("langue").textContent = choixLangue;
+
+  // Variable Ruban choix de la monnaie
+//-------------------------------------
+
+  let choixMonnaie = "EURO €";
+  document.getElementById("monnaie").textContent = choixMonnaie;
+
