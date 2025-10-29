4188c4d6fcf9e4686b33c96cff044c9215263792
Yannick Franchaisse <yannick.franchaisse@gmail.com>
2025-10-29 12:36:43 +0100

    NavBar

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -5,9 +5,23 @@
   let choixLangue = "FRANCAIS (Fr)";
   document.getElementById("langue").textContent = choixLangue;
 
-  // Variable Ruban choix de la monnaie
+// Variable Ruban choix de la monnaie
 //-------------------------------------
 
   let choixMonnaie = "EURO €";
   document.getElementById("monnaie").textContent = choixMonnaie;
 
+
+// Gestion du menu hamburger
+//-------------------------------------
+document.addEventListener('DOMContentLoaded', function() {
+  const hamburgerButton = document.querySelector('.navbar-toggler');
+  const menuPrincipal = document.getElementById('menuPrincipal');
+  
+  if (hamburgerButton && menuPrincipal) {
+    hamburgerButton.addEventListener('click', function() {
+      // affiche/masque le menu
+      menuPrincipal.classList.toggle('show');
+    });
+  }
+});
